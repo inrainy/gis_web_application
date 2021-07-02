94a19ffc5104b4bc9b885e7a2fdfe02750c1265a
inrainy <86110182+inrainy@users.noreply.github.com>
2021-07-02 10:12:50 +0900

    7.2.webapplication.1

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def hello_world(request):
-    return HttpResponse('hello world!')
+    return render(request, 'base.html')
     #대소문자 틀리면 안됨 HttpResponse
     #http에서 alt + enter누르면 뭐가 뜸
