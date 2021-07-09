f7b6b8be053fb5b3cd29dd5d51517c00605242fa
inrainy <86110182+inrainy@users.noreply.github.com>
2021-07-09 10:23:56 +0900

    7.9.webapplication4

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,6 +5,12 @@ from django.shortcuts import render
 
 
 def hello_world(request):
-    return render(request, 'accountapp/hello_world.html')
+    if request.method == "POST":
+        return render(request, 'accountapp/hello_world.html',
+                      context={'text' : 'POST METHOD!'})
+
+    else:
+        return render(request, 'accountapp/hello_world.html',
+                      context={'text' : 'GET METHOD!'})
     #대소문자 틀리면 안됨 HttpResponse
     #http에서 alt + enter누르면 뭐가 뜸
