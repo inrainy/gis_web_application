6b1612b266fa2b824e05038f2f1fea6edfd20689
inrainy <86110182+inrainy@users.noreply.github.com>
2021-07-13 09:26:14 +0900

    7.13.webapplication5

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,15 +2,23 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from accountapp.models import NewModel
 
 
 def hello_world(request):
     if request.method == "POST":
+
+        temp = request.POST.get('input_text')
+
+        model_instance = NewModel()
+        model_instance.text = temp
+        model_instance.save()
+
         return render(request, 'accountapp/hello_world.html',
-                      context={'text' : 'POST METHOD!'})
+                      context={'model_instance':model_instance})
 
     else:
         return render(request, 'accountapp/hello_world.html',
-                      context={'text' : 'GET METHOD!'})
+                      context={'text':'GET METHOD!'})
     #대소문자 틀리면 안됨 HttpResponse
     #http에서 alt + enter누르면 뭐가 뜸
