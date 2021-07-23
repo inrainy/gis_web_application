5e877eaadefeacc193ef36895a7b403287dde52a
inrainy <86110182+inrainy@users.noreply.github.com>
2021-07-23 10:15:19 +0900

    7.23.webapplication10

--- web_app_study_6_30/settings.py
+++ web_app_study_6_30/settings.py
@@ -55,7 +55,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap4',
-    'accountapp'
+    'accountapp',
+    'profileapp',
 ]
 
 MIDDLEWARE = [
